e89b4fb8d49270b689129fb7da14f21fcff26206
cosmo2097 <zhangchao51@163.com>
2011-11-17 21:54:59 +0800

    add unicorn config and fix email config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,17 @@ Maitianer::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'www.maitianer.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address  => "smtp.gmail.com",
+    :port     => 587,
+    :domain   => 'maitianer.com',
+    :user_name => 'zhangchao51@gmail.com',
+    :password => 'zc19860721',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Enable threaded mode
   # config.threadsafe!
